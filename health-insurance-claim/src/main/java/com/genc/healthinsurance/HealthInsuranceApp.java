638eb6294e7ac13de1f4d176d1c201c5b0986f0d
2440888 <2440888@LTIN649613.cts.com>
2025-09-16 13:47:15 +0530

    Spring skeleton for backend in eclipse

---
+++ health-insurance-claim/src/main/java/com/genc/healthinsurance/HealthInsuranceApp.java
@@ -0,0 +1,10 @@
+package com.genc.healthinsurance;
+
+public class HealthInsuranceApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- health-insurance-claim/src/test/java/health_insurance_claim_management_system/health_insurance_claim/AppTest.java
+++
@@ -1,5 +0,0 @@
-package health_insurance_claim_management_system.health_insurance_claim;
-
-public class AppTest {
-	// Add your test cases here
-}
