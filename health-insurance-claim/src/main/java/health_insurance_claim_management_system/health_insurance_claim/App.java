98b7cd821a2fef78739c8600e9aff18ef9cbd56c
2440888 <2440888@LTIN649613.cts.com>
2025-09-16 08:51:56 +0530

    eclipse backend v-1

---
+++ health-insurance-claim/src/main/java/health_insurance_claim_management_system/health_insurance_claim/App.java
@@ -0,0 +1,13 @@
+package health_insurance_claim_management_system.health_insurance_claim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
