04c79c25f81882493a0b1b61ae8582b4431e0db8
VishalKushwaha22 <VishalKumar.Kushwaha@cognizant.com>
2025-09-26 12:14:58 +0530

    Delete frontend directory

--- frontend/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-     $(function(){
-      $("#navbar").load("/frontend/components/navbar.html");
-      $("#modules").load("/frontend/components/modules.html");
-      $("#footer").load("/frontend/components/footer.html");
-    });
-
-    function toggleDarkMode() {
-      document.body.classList.toggle("dark-mode");
-    }
